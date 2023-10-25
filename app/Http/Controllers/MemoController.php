e93271b3e20e39248ecc4fc76e2a51c8d33a5943
Artem <korwin13@gmail.com>
2023-10-25 22:37:02 +0300

    Memo API resource endpoint. Authorization, validation.

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreMemoRequest;
-use App\Http\Requests\UpdateMemoRequest;
+use Illuminate\Http\Request;
+use App\Http\Resources\MemoResource;
+use App\Http\Resources\MemoCollection;
 use App\Models\Memo;
+use Auth;
 
 class MemoController extends Controller
 {
@@ -13,54 +15,70 @@ class MemoController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        $user_id = Auth::user()->id;
+        return new MemoCollection(Memo::where('user_id', $user_id)->paginate(4));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreMemoRequest $request)
+    public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required',
+            'text' => 'nullable',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $validated['user_id'] = Auth::user()->id;
+
+        $memo = Memo::create($validated);
+        return response()->json(['status' => 'Memo created.']);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Memo $memo)
+    public function show(string $id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Memo $memo)
-    {
-        //
+        // /TODO: 404 instead of Unauthenticated
+        // /TODO: 404 when not found
+        // abort(404);
+        // try
+        return new MemoResource(Memo::findOrFail($id));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateMemoRequest $request, Memo $memo)
+    public function update(Request $request, string $id)
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required',
+            'text' => 'nullable',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $memo = Memo::findOrFail($id); // 404 sent back if not found
+        // check if user is owner of Memo, else return 404
+        if( $memo->user_id != Auth::user()->id ) {
+            abort(404);
+        }
+
+        $memo->update($validated);
+        return response()->json(['status' => 'Memo updated.']);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Memo $memo)
+    public function destroy(string $id)
     {
-        //
+        $memo = Memo::findOrFail($id); // 404 sent back if not found
+        if( $memo->user_id != Auth::user()->id ) {
+            abort(404);
+        }
+        $memo->delete();
+        return response()->json(['status' => 'Memo deleted.']);        //
     }
 }
--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Memo extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'title',
+        'text',
+        'due_date',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'due_date'   => 'datetime:Y-m-d H:i:s',
+    ];
 }
